1d4ca0f53f2a8e704d10e365f7e2d18220e02ad6
Lin <linpan0328@gmail.com>
2021-02-19 13:31:26 -0500

    Fix nullability issues v2

--- src/main/kotlin/me/backword/pluginapi/commands/arguments/Arguments.kt
+++ src/main/kotlin/me/backword/pluginapi/commands/arguments/Arguments.kt
@@ -1,6 +1,7 @@
 package me.backword.pluginapi.commands.arguments
 
 import me.backword.pluginapi.commands.exception.CommandException
+import org.jetbrains.annotations.NotNull
 
 class Arguments(private val args: Array<String>) {
     private var index = 0
@@ -8,8 +9,8 @@ class Arguments(private val args: Array<String>) {
     fun length() = args.size
     fun next() = args[index++]
 
-    fun <T> next(resolver: ArgumentResolver<T>, exception: CommandException): T = next(resolver) ?: throw exception
-    fun <T> next(resolver: ArgumentResolver<T>, error: String): T = next(resolver, CommandException(error))
+    @NotNull fun <T> next(resolver: ArgumentResolver<T>, error: String) = next(resolver, CommandException(error))
+    @NotNull fun <T> next(resolver: ArgumentResolver<T>, exception: CommandException) = next(resolver) ?: throw exception
     fun <T> next(resolver: ArgumentResolver<T>): T? {
         try {
             if (args.isEmpty()) return null
@@ -19,15 +20,15 @@ class Arguments(private val args: Array<String>) {
         }
     }
 
-    fun nextString(exception: CommandException) = nextString() ?: throw exception
-    fun nextString(error: String) = nextString(CommandException(error))
+    @NotNull fun nextString(error: String) = nextString(CommandException(error))
+    @NotNull fun nextString(exception: CommandException) = nextString() ?: throw exception
     fun nextString(): String? {
         if (args.isEmpty()) return null
         return next()
     }
 
-    fun join(delimiter: String, exception: CommandException) = join(delimiter) ?: exception
-    fun join(delimiter: String, error: String) = join(delimiter, CommandException(error))
+    @NotNull fun join(delimiter: String, error: String) = join(delimiter, CommandException(error))
+    @NotNull fun join(delimiter: String, exception: CommandException) = join(delimiter) ?: exception
     fun join(delimiter: String): String? {
         val joined = args.copyOfRange(index, args.size).joinToString(delimiter).trim()
         return if (joined.isEmpty()) null else joined
