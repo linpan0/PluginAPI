5724e840f6898783661394a19021704bee401d54
Backword <linpan0328@gmail.com>
2021-07-04 13:22:50 -0400

    Fix spaces

--- src/main/kotlin/me/backword/pluginapi/menu/StandardMenu.kt
+++ src/main/kotlin/me/backword/pluginapi/menu/StandardMenu.kt
@@ -9,15 +9,15 @@ import org.bukkit.inventory.ItemStack
 open class StandardMenu(private val name: String, private val size: Int) : Menu {
     private val buttons = HashMap<Int, Button>(size)
 
-    fun button(itemStack: ItemStack, action: ((InventoryClickEvent) -> Unit)? = { it.isCancelled = true} ) {
+    fun button(itemStack: ItemStack, action: ((InventoryClickEvent) -> Unit)? = { it.isCancelled = true } ) {
         val firstEmpty = (0..size).firstOrNull { buttons[it] === null }
         if (firstEmpty !== null)
             buttons[firstEmpty] = Button(itemStack, action)
     }
 
-    fun button(index: Int, itemStack: ItemStack, action: ((InventoryClickEvent) -> Unit)? = { it.isCancelled = true} ) = buttons.set(index, Button(itemStack, action))
+    fun button(index: Int, itemStack: ItemStack, action: ((InventoryClickEvent) -> Unit)? = { it.isCancelled = true } ) = buttons.set(index, Button(itemStack, action))
 
-    fun button(x: Int, y: Int, itemStack: ItemStack, action: ((InventoryClickEvent) -> Unit)? = { it.isCancelled = true} ) = buttons.set(y * 9 + x, Button(itemStack, action))
+    fun button(x: Int, y: Int, itemStack: ItemStack, action: ((InventoryClickEvent) -> Unit)? = { it.isCancelled = true } ) = buttons.set(y * 9 + x, Button(itemStack, action))
 
     // Clicked item should never be null(so not Material.AIR), so a button theoretically should always exist
     override fun getButton(slot: Int) = buttons[slot]
