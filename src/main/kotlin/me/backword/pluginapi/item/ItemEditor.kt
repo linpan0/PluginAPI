22bc9550ded5cc49c865e3526f5699d4cbf4d2d9
Backword <linpan0328@gmail.com>
2021-07-04 19:39:11 -0400

    Added ItemEditor to easily edit properties of an ItemStack

---
+++ src/main/kotlin/me/backword/pluginapi/item/ItemEditor.kt
@@ -0,0 +1,21 @@
+package me.backword.pluginapi.item
+
+import net.kyori.adventure.text.Component
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
+
+// TODO: 7/4/2021 add more functions here
+
+fun <T : ItemMeta> ItemStack.editMeta(action: (T) -> Unit) {
+    val meta = itemMeta as T
+    action(meta)
+    itemMeta = meta
+}
+
+fun ItemStack.editLore(action: (MutableList<Component>) -> Unit) = editMeta<ItemMeta> { meta ->
+    val lore = meta.lore() ?: arrayListOf()
+    action(lore)
+    meta.lore(lore)
+}
+
+fun ItemStack.addLore(component: Component) = editLore { it.add(component) }
