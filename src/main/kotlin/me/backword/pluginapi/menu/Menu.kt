e7fa5cfe65dc11acf80b66e4401dee40905ccb2c
Backword <linpan0328@gmail.com>
2021-07-06 19:03:04 -0400

    Forgot to add a few things to Menu, also still need to add PaginatedMenu(TODO)

--- src/main/kotlin/me/backword/pluginapi/menu/Menu.kt
+++ src/main/kotlin/me/backword/pluginapi/menu/Menu.kt
@@ -14,13 +14,25 @@ interface Menu : InventoryHolder {
                 if (event.clickedInventory === null) return@listen
                 if (event.currentItem === null) return@listen
                 val menu = event.clickedInventory?.holder as? Menu ?: return@listen
+                menu.onClick(event)
                 val action = menu.getButton(event.slot)?.action ?: return@listen
                 action(event)
+            }
+
+            listen<InventoryOpenEvent>(plugin) { event ->
+                val menu = event.inventory.holder as? Menu ?: return@listen
+                menu.onOpen(event)
+            }
+
+            listen<InventoryCloseEvent>(plugin) { event ->
+                val menu = event.inventory.holder as? Menu ?: return@listen
+                menu.onClose(event)
             }
         }
     }
 
     fun getButton(slot: Int): Button?
+    fun onClick(event: InventoryClickEvent) {}
     fun onOpen(event: InventoryOpenEvent) {}
     fun onClose(event: InventoryCloseEvent) {}
 }
