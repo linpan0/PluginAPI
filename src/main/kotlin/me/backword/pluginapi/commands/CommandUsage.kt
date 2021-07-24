e38d5727cbea9530c6baa3912672d9a161bb44f1
Bqckword <linpan0328@gmail.com>
2021-07-24 14:12:35 -0400

    Maybe fixed all messages

--- src/main/kotlin/me/backword/pluginapi/commands/CommandUsage.kt
+++ src/main/kotlin/me/backword/pluginapi/commands/CommandUsage.kt
@@ -7,12 +7,11 @@ class CommandUsage(private vararg val args: Argument) {
     fun simple() = "§7/${aliases.joinToString("|")} ${args.joinToString(" ") { it.formattedName() }}"
 
     fun detailed() = buildString {
-        appendLine("§7§m----------§7§l/${aliases[0]} Command Help§7§m----------")
-        appendLine("§eAliases: §7${aliases.drop(1).joinToString(", ")}")
-        appendLine("§eDescription: §7$description")
-        appendLine("§7§nArguments")
+        appendLine("§8§m-----------§7§l/${aliases[0]} Command Help§8§m-----------")
+        appendLine("§e§lAliases§7: ${aliases.drop(1).joinToString(", ")}")
+        appendLine("§e§lDescription§7: $description")
+        appendLine("§7§l§nArguments")
         args.map(Argument::detailed).forEach(::appendLine)
-        appendLine("§7§m--------------------------------------------------")
     }
 }
 
@@ -23,10 +22,10 @@ interface Argument {
 
 class RequiredArgument(private val name: String, private val description: String) : Argument {
     override fun formattedName() = "§e<$name>"
-    override fun detailed() = "§e$name: §7$description"
+    override fun detailed() = "§e§l$name§7: $description"
 }
 
 class OptionalArgument(private val name: String, private val description: String, private val default: String? = null) : Argument {
     override fun formattedName() = "§e($name)"
-    override fun detailed() = "§e$name${if (default == null) "" else "($default)"}: §7$description"
+    override fun detailed() = "§e§l$name${if (default == null) "" else "§8($default)"}§7: $description"
 }
